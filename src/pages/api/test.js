c755ec7635157fd1bb9d6532e1e688ce3606cf1f
tomoki sugihara <sugihara.10969@gmail.com>
2021-06-09 14:59:41 +0900

    Test functions hello world

---
+++ src/pages/api/test.js
@@ -0,0 +1,8 @@
+const handler = (req, res) => {
+  fetch("https://github.com/Tomoki-Sugihara").then(console.log)
+  res.status(200).json({ text: "Hello" })
+  res.end()
+}
+
+// eslint-disable-next-line import/no-default-export
+export default handler
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,31 @@
+import { Suspense, useEffect, useState } from "react"
 import { Layout } from "src/components/layout"
 
-const Login = () => {
+// export const getServerSideProps = async () => {
+//   const res = await axios.get('api/test')
+//   // const res = await fetch('api/test')
+//   // const data = await res.json()
+
+//   return {
+//     props: {
+//       serverText: res.data.text,
+//     },
+//   }
+// }
+
+const Login = (props: { serverText: string }) => {
+  const [text, setText] = useState("")
+  console.log((process as any).client)
+  useEffect(() => {
+    fetch("/api/test").then(async (res: any) => {
+      const data = await res.json()
+      console.log(data)
+      setText(data.text)
+    })
+  }, [])
   return (
     <Layout>
+      {text}
       <button>ログイン</button>
     </Layout>
   )
